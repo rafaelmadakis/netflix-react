26c82b047344061aca453faa66cd77d6cd929090
rafaelmadakis <rafaelmadakis@gmail.com>
2020-08-24 20:57:45 -0300

    Inclusão de nomes, imagens e descrição da API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState}  from 'react';
 import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeatureMovie from './components/FeatureMovie';
 
-function App() { 
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeatureData] = useState([null]);
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      // Pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegando o feature
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(chosenInfo);
+
+
+    }
+
+    loadAll();
+  }, []);
+
+
   return (
-    <div className="App"> 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="page">
 
-export default App;
+    {featuredData &&
+      <FeatureMovie item={featuredData} />
+    }
+
+     <section className="lists">
+       {movieList.map((item, key)=>(
+        <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+     </section>
+    </div>
+  )
+};
---
+++ src/components/FeatureMovie.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './FeatureMovie.css';
+
+export default ({item}) => {
+    return (
+        <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(http://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className="featured--vertical">
+                <div className="featured--horizontal">
+                    <div className="featured--name">{item.original_name}</div>
+                    <div className="featured--info">
+                        <div className="featured--points">{item.vote_average} pontos</div>
+                        <div className="featured--year">2099</div>
+                        <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                        <div className="featured--descrition">{item.overview}</div>
+                        <div className="featured--buttons">
+
+
+                        </div>
+                        <div className="featured--genres"><strong>Gêneros:</strong> ...</div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    );
+}
